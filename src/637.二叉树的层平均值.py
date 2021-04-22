c5aedba7b4df2ceff0ea4130abac040406521c13
xing <wangxing1027@gmail.com>
2021-04-22 15:26:26 +0800

    test

---
+++ src/637.二叉树的层平均值.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=637 lang=python
+#
+# [637] 二叉树的层平均值
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+# @lc code=end
+
