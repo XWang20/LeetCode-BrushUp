00421e12234928f395b33ea9b86cdcf0121a6f30
xing <wangxing1027@gmail.com>
2021-05-09 12:43:21 +0800

    5.9 update

---
+++ src/151.翻转字符串里的单词.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=151 lang=python
+#
+# [151] 翻转字符串里的单词
+#
+
+# @lc code=start
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = s.split()
+        s.reverse()
+        return ' '.join(s)
+        
+# @lc code=end
+
